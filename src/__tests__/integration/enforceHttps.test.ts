2db57380e7542f3fb122c6c759f6371200584c93
cmillward13 <chris.millward13@hotmail.com>
2024-11-03 20:55:40 -0500

    Updated testing suite for enforceHttps middleware.

--- src/__tests__/integration/enforceHttps.test.ts
+++ src/__tests__/integration/enforceHttps.test.ts
@@ -4,6 +4,7 @@ import request from 'supertest';
 import https from 'https';
 import fs from 'fs';
 import { join } from 'path';
+import { config } from '../../config/env';
 
 // Mock the config
 jest.mock('../../config/env', () => ({
@@ -27,6 +28,7 @@ createSelfSignedCert();
 
 const key = fs.readFileSync(join(__dirname, 'test certs', 'key.pem'));
 const cert = fs.readFileSync(join(__dirname, 'test certs', 'cert.pem'));
+const httpsAgent = new https.Agent({ca: cert, rejectUnauthorized: false});
 
 describe('enforceHttps Middleware', () => {
     let app: Express;
@@ -47,34 +49,96 @@ describe('enforceHttps Middleware', () => {
         jest.clearAllMocks();
     });
 
-    // Use-case 1: HTTPS request in production
-    it('should response with 200 OK if HTTPS policy used in production', async () => {
-        const httpsAgent = new https.Agent({
-            ca: cert,
-            rejectUnauthorized: false
-        });
+    // Use-case 1: HTTPS in production
+    it('should call next() and return 200 if the request is HTTPS in production', async () => {
+        const redirectSpy = jest.spyOn(express.response, 'redirect');
+
+        const response = await request(httpsServer)
+            .post('/test')
+            .agent(httpsAgent);
+
+        expect(redirectSpy).toHaveBeenCalledTimes(0);
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Success');
+    });
+
+    // Use-case 2: HTTP in production
+    it('should call res.redirect() if the request is HTTP in production', async () => {
+        const redirectSpy = jest.spyOn(express.response, 'redirect');
+
+        const response = await request(app)
+            .post('/test');
+
+        expect(redirectSpy).toHaveBeenCalledTimes(1);
+        expect(response.status).toBe(302);
+    });
+    // Use-case 3: HTTPS from proxy in production
+    it('should call next() and return 200 if the request is HTTPS from a proxy in production', async () => {
+        const redirectSpy = jest.spyOn(express.response, 'redirect');
 
         const response = await request(httpsServer)
             .post('/test')
             .set('X-Forwarded-Proto', 'https')
             .agent(httpsAgent);
 
-
+        expect(redirectSpy).toHaveBeenCalledTimes(0);
         expect(response.status).toBe(200);
         expect(response.body.message).toBe('Success');
     });
 
-    // Use-case 2: HTTP request in production
-    it('should respond with 302 (redirect) if HTTP policy used in production', async () => {
-        const response = await request(app)
-            .post('/test');
+    // Use-case 4: HTTPS request in development
+    it('should call next() and return 200 if the request is HTTPS in development', async () => {
+        const redirectSpy = jest.spyOn(express.response, 'redirect');
+        config.environment = 'development'
 
+        const response = await request(httpsServer)
+            .post('/test')
+            .agent(httpsAgent);
+
+        expect(redirectSpy).toHaveBeenCalledTimes(0);
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Success');
+    });
+
+    // Use-case 5: HTTP request from localhost in development
+    it('should call next() and return 200 if the request is HTTP from localhost in development', async () => {
+        const redirectSpy = jest.spyOn(express.response, 'redirect');
+        config.environment = 'development';
+
+        const response = await request(app)
+            .post('/test')
+            .set('Host', 'localhost');
+
+        expect(redirectSpy).toHaveBeenCalledTimes(0);
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Success');
+    });
+
+    // Use-case 6: HTTP request in development (non-localhost)
+    it('should call res.redirect() if the request is HTTP in development (non-localhost)', async () => {
+        const redirectSpy = jest.spyOn(express.response, 'redirect');
+        config.environment = 'development';
+
+        const response = await request(app)
+            .post('/test')
+            .set('Host', 'example.com');
+
+        expect(redirectSpy).toHaveBeenCalledTimes(1);
         expect(response.status).toBe(302);
     });
 
-    // Use-case 3: HTTPS request in development
+    // Use-case 7: HTTPS request from proxy in development
+    it('should call next() and return 200 if the request is HTTPS from a proxy in development', async () => {
+        const redirectSpy = jest.spyOn(express.response, 'redirect');
+        config.environment = 'development';
 
-    // Use-case 4: HTTP request from localhost in development
+        const response = await request(httpsServer)
+            .post('/test')
+            .set('X-Forwarded-Proto', 'https')
+            .agent(httpsAgent);
 
-    // Use-case 5: HTTP request in development (non-localhost)
+        expect(redirectSpy).toHaveBeenCalledTimes(0);
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Success');
+    });
 });
